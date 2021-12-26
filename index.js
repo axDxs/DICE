ef0ae4f675d6bbb99241c63c3e161bf3dfc39be2
axDxs <82862098+axDxs@users.noreply.github.com>
2021-12-26 16:13:56 +0530

    DiceImage

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+
+randomDiceImage = "images/" + "dice" + randomNumber1 + ".png";
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomDiceImage);
+
+
+
+
